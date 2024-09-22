4f769df250f2f3863463a4dfa40801c367d744c0
aria <aria.legion2@gmail.com>
2024-09-22 18:00:16 +0330

    debuged user_app models fle

--- bottle_proj/user_app/models.py
+++ bottle_proj/user_app/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import User
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.user.username
 # Create your models here.
