ea405af42edf6ea54c9f5e5b6a2d4d412158f89b
aria <aria.legion2@gmail.com>
2024-09-22 17:59:37 +0330

    added first views

--- bottle_proj/message_app/views.py
+++ bottle_proj/message_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Message , Conversation
+from .serializers import MessageSerializer , ConversationSerializer
+from rest_framework import generics
 
+class ConversationsList(generics.ListAPIView):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
 # Create your views here.
--- bottle_proj/user_app/views.py
+++ bottle_proj/user_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .serializers import UserSerializer
+from rest_framework.generics import CreateAPIView
+from .models import Account
+class Register(CreateAPIView):
+    pass
 # Create your views here.
