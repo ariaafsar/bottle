5b8205b92ce69771127d8fb4945236625a797bc0
aria <aria.legion2@gmail.com>
2024-09-24 18:29:26 +0330

    added first version of user models

--- bottle_proj/user_app/models.py
+++ bottle_proj/user_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bcoin = models.IntegerField(default=100)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    max_daily_bottle = models.IntegerField(default=3)
+    reply_able = models.BooleanField(default=False)
+    read_bottles = models.IntegerField(default=0)
+    
 
 # Create your models here.
