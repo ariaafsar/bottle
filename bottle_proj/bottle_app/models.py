e2294cfcfce0bceea55210a9e4078302953b382f
aria <aria.legion2@gmail.com>
2024-09-24 18:29:54 +0330

    added first version of bottle models

--- bottle_proj/bottle_app/models.py
+++ bottle_proj/bottle_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user_app.models import Account
 
+class Bottle(models.Model):
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE)
+    reciver = models.ForeignKey(Account, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_and_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sender.user.username
 # Create your models here.
