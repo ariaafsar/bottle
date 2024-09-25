fdf4637281182cca03271a7b8c833d3aa4777c21
aria <aria.legion2@gmail.com>
2024-09-25 17:46:40 +0330

    debuged models

--- bottle_proj/bottle_app/models.py
+++ bottle_proj/bottle_app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from user_app.models import Account
 
 class Bottle(models.Model):
-    sender = models.ForeignKey(Account, on_delete=models.CASCADE)
-    reciver = models.ForeignKey(Account, on_delete=models.CASCADE)
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE , related_name='sender')
+    reciver = models.ForeignKey(Account, on_delete=models.CASCADE , related_name='reciver')
     content = models.TextField()
     date_and_time = models.DateTimeField(auto_now_add=True)
 
