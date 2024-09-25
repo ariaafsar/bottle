16ddbd3ae5e9ead510f802ab06d76f4978b17003
aria <aria.legion2@gmail.com>
2024-09-25 17:48:12 +0330

    debuged models file

--- bottle_proj/user_app/models.py
+++ bottle_proj/user_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Account(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE , related_name='user')
     bcoin = models.IntegerField(default=100)
     x = models.IntegerField()
     y = models.IntegerField()
