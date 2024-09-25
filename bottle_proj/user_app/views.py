a8c3775fde67218c91c682806faa37b68d757290
aria <aria.legion2@gmail.com>
2024-09-25 17:19:05 +0330

    added user app vews

--- bottle_proj/user_app/views.py
+++ bottle_proj/user_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics , status
+from .models import Account
+from .serializers import AccountSerializer
+from rest_framework.response import Response
+from rest_framework_simplejwt.view import TokenObtainPairView , TokenRefreshView
 
+class Login(TokenObtainPairView):
+    pass
+
+class Refresh(TokenRefreshView):
+    pass
+
+class Signup(generics.CreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    def Create(self , request , *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 # Create your views here.
