fb4ed3f13785be98cdb18a8c38345f13aae96de0
aria <aria.legion2@gmail.com>
2024-09-25 21:01:10 +0330

    edited models file

--- bottle_proj/bottle_app/models.py
+++ bottle_proj/bottle_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from user_app.models import Account
 
+class ShopBottle(models.Model):
+    price = models.IntegerField()
+    bottle_range = models.IntegerField()
+    content_legth = models.IntegerField()
+
+    def __str__(self):
+        return {(self.price) - (self.bottle_range) - (self.content_legth)}
+    
 class Bottle(models.Model):
     sender = models.ForeignKey(Account, on_delete=models.CASCADE , related_name='sender')
     reciver = models.ForeignKey(Account, on_delete=models.CASCADE , related_name='reciver')
