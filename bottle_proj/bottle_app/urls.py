06b438bba02eb0db1230fde45ec19a645abf44bc
aria <aria.legion2@gmail.com>
2024-09-25 22:51:05 +0330

    added urls

--- bottle_proj/bottle_app/urls.py
+++ bottle_proj/bottle_app/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path 
+from .views import SendBottle , BottleShopList , BuyBottle
+
+urlpatterns = [
+    path('sendbottle' , SendBottle.as_view()),
+    path('bottleshoplist' , BottleShopList.as_view()),
+    path('buybottle' , BuyBottle.as_view())
+]
--- bottle_proj/user_app/urls.py
+++ bottle_proj/user_app/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path 
+from .views import Signup , Login , Refresh
+
+urlpatterns = [
+    path('Signup'  , Signup.as_view()),
+    path('Login' , Login.as_view()),
+    path('Refresh' , Refresh.as_view()),
+]
