5639e0f12a4bb82215d66ea088fe564a3303a64c
aria <aria.legion2@gmail.com>
2024-09-25 21:01:29 +0330

    added some serializers

--- bottle_proj/bottle_app/serializers.py
+++ bottle_proj/bottle_app/serializers.py
@@ -1,3 +1,20 @@
 from rest_framework import serializers
 from .models import Bottle
 
+class ShopBottleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bottle
+        fields = '__all__'
+
+class BottleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bottle
+        fields = ['id' , 'message']
+
+class OwnedBottleSerializer(serializers.ModelSerializer):
+    price = serializers.IntegerField(source='bought_bottle.price')
+    range = serializers.IntegerField(source='bought_bottle.range')
+
+    class Meta:
+        model = Bottle
+        fields = ['price', 'range']
